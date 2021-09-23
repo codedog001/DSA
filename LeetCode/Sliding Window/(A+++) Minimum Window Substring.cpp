ba8e1b6297bf936f31222b2664ccf7a802daa477
Mohit Jain <70198503+codedog001@users.noreply.github.com>
2021-09-24 00:03:16 +0530

    Leetcode Sliding Window

---
+++ LeetCode/Sliding Window/(A+++) Minimum Window Substring.cpp
@@ -0,0 +1,58 @@
+Doing a dry run of this code, would probably be the best way to understand it real quick.
+A++
+
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int i=0,j=0,MinL=INT_MAX,start=0;
+        unordered_map<char,int> mp;
+        for(auto &g:t)
+            mp[g]++;
+        int count=mp.size();
+        
+        if(mp.find(s[j])!=mp.end()){
+            mp[s[j]]--;
+            if(mp[s[j]]==0)
+                count--;
+        }
+        
+        while(j<s.length()){
+            if(count>0){
+                j++;
+                if(mp.find(s[j])!=mp.end()){
+                    mp[s[j]]--;
+                    if(mp[s[j]]==0)
+                    count--;
+                }
+            }
+            else if(count==0){
+                // MinL=min(MinL,j-i+1);
+                if(MinL>j-i+1){
+                    MinL=j-i+1;
+                    start=i;
+                }
+                while(count==0){
+                    if(mp.find(s[i])!=mp.end()){
+                        mp[s[i]]++;
+                        if(mp[s[i]]==1){
+                            count++;
+                            // MinL=min(MinL,j-i+1);
+                             if(MinL>j-i+1){
+                                MinL=j-i+1;
+                                start=i;
+                            } 
+                        }
+                        i++;
+                    }
+                    else
+                        i++;
+                }
+            }
+        }
+        string str="";
+        if(MinL!=INT_MAX)
+            return str.append(s.substr(start,MinL));
+        else
+            return str;
+    }
+};
