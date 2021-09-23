ed851da6da46652f6153a03361c97208ef60c55a
Mohit Jain <70198503+codedog001@users.noreply.github.com>
2021-09-23 13:58:01 +0530

    gfg sliding window

---
+++ Not AC/Correct code not AC/Longest K unique characters substring.cpp
@@ -0,0 +1,29 @@
+int longestKSubstr(string a, int k) {
+    // your code here
+    unordered_map<char,int> m;
+    int i=0,j=0,maxLen=-1;
+    
+    while(j<a.size()){
+        //1. Calculation
+        m[a[j]]++;
+        
+        if(m.size()<k) j++;
+        
+        else if(m.size() == k){
+            int numOfUniqueChar = j-i+1;
+            maxLen = (maxLen, numOfUniqueChar);
+            j++;
+        }
+        
+        else if(m.size() >k){
+            while(m.size() > k){
+                m[a[i]]--;
+                if(m[a[i]] == 0) m.erase(a[i]);
+                i++;
+            }
+            j++;
+        }
+    }
+
+    return maxLen;
+    }
