adb3509d6b0602c8195008a225d41233476bbd98
Mohit Jain <70198503+codedog001@users.noreply.github.com>
2021-10-13 12:55:33 +0530

    BST Leetcode

---
+++ LeetCode/Binary Search Tree/Binary Search Used/Create BST from sorted array.cpp
@@ -0,0 +1,14 @@
+BinaryTreeNode<int>* helper(int* a, int si, int ei){
+    if(si>ei) return NULL;
+    int mid = (si+ei)/2;
+    BinaryTreeNode<int>* root = new BinaryTreeNode<int>(a[mid]);
+    root->left = helper(a,si,mid-1);
+    root->right = helper(a,mid+1,ei);
+    return root;
+}
+
+BinaryTreeNode<int>* constructTree(int *input, int n) {
+	// Write your code here
+    if(n<=0) return NULL;
+    return helper(input,0,n-1);
+}
