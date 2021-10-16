ab79936c2c9d902536fe33a31090b84a931545ea
Mohit Jain <70198503+codedog001@users.noreply.github.com>
2021-10-16 20:07:43 +0530

    Leetcode BST

--- LeetCode/Binary Search Tree/LCA of BT and BST/LCA of BST.cpp
+++ LeetCode/Binary Search Tree/LCA of BT and BST/LCA of BST.cpp
@@ -1,6 +1,7 @@
+
 //Logic: The key is, when p and q are on other side of current node, then the current node
 // will be the LCA, if current node is not LCA, p and q will always be on same side of 
-//current node.
+// current node.
 
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(root->val > p->val && root->val > q->val) return lowestCommonAncestor(root->left, p, q);
