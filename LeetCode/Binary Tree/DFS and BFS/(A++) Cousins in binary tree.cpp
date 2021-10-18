e7eeaf1d7d919e7ba756be1c9d8c20c6667780c7
Mohit Jain <70198503+codedog001@users.noreply.github.com>
2021-10-18 12:15:27 +0530

    Leetcode Binary Tree

---
+++ LeetCode/Binary Tree/DFS and BFS/(A++) Cousins in binary tree.cpp
@@ -0,0 +1,62 @@
+Logic: If we find one of the nodes having value x or y, say we found x, we make the other one i.e. y = toSearch, when we find both x and y, we have to check whether they are siblings, if they are not siblings, return true, else false.
+
+Note: Code is long, because it is very easy to understand, I'm not a fan of cryptic code.
+
+   bool isCousins(TreeNode* root, int x, int y) {
+        if(!root || root-> val == x || root -> val == y) return false;
+        queue<TreeNode*> q;
+        
+        int toSearch = -1;
+        q.push(root);
+        while(!q.empty()){
+            int n=q.size();
+            vector<TreeNode*> v;
+            
+            for(int i=0; i<n; i++){
+                auto front = q.front();
+                q.pop();
+                TreeNode* l = NULL, *r=NULL; 
+                
+                if(front->left) {
+                    l=front->left;
+                    q.push(l);
+                    if(!v.empty()){
+                        if(l-> val == toSearch && !sibling(front,l,v[0])) return true;
+                    }else{
+                        if(l->val == x) {
+                        v.push_back(l);
+                        toSearch = y;
+                        }
+                        else if(l->val == y){
+                            v.push_back(l);
+                            toSearch = x;
+                        } 
+                    }
+                }
+                if(front -> right) {
+                    r =  front -> right;
+                    q.push(r);
+                    if(!v.empty()){
+                        if(r-> val == toSearch && !sibling(front,v[0],r)) return true;
+                    }else{
+                        if(r->val == x) {
+                        v.push_back(r);
+                        toSearch = y;
+                        }
+                        else if(r->val == y){
+                            v.push_back(r);
+                            toSearch = x;
+                        } 
+                    }
+                }
+            }
+			//If v.size() == 1, it implies, we traversed one level completely and only one of x or y was found, so they don't exist in same level, hence return false.
+            if(v.size()==1) return false;
+        }
+        return false;
+    }
+	
+	bool sibling(TreeNode* root, TreeNode* l, TreeNode* r){
+        if((root->left == l && root->right == r) || (root->right==l && root->left == r)) return true;
+        return false;
+    }
