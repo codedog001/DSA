89a4670457f241659382820301a917bf514625b7
Mohit Jain <70198503+codedog001@users.noreply.github.com>
2021-10-20 23:40:57 +0530

    Leetcode BST

---
+++ LeetCode/Binary Tree/Exceptional Problems/(A+++++) Binary tree to doubly Linked list.cpp
@@ -0,0 +1,28 @@
+    node* bd(node* root){
+    // Base case
+    if (root == NULL)
+        return root;
+ 
+    // Convert the left subtree and link to root
+    if (root->left != NULL) {
+        node* left = bd(root->left);
+ 
+        while(left->right) left=left->right;
+ 
+        left->right = root;
+ 
+        root->left = left;
+    }
+ 
+    if (root->right != NULL) {
+        node* right = bd(root->right);
+ 
+        while(right->left) right = right -> left;
+ 
+        right->left = root;
+ 
+        root->right = right;
+    }
+ 
+    return root;
+}
