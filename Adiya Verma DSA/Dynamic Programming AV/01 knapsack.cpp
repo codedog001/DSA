7b893d59e0c4bed6a8ae1d0a80d6fbb58627e8b5
Mohit Jain <70198503+codedog001@users.noreply.github.com>
2021-11-05 22:54:59 +0530

    01 Knapsack GFG

--- Adiya Verma DSA/Dynamic Programming AV/01 knapsack.cpp
+++ Adiya Verma DSA/Dynamic Programming AV/01 knapsack.cpp
@@ -48,3 +48,28 @@ int knapSack(int w, int wt[], int val[], int n){
         }
         
     }
+
+//3. Bottom Up Approach - Dynamic Programming
+int knapSack(int w, int wt[], int val[], int n) 
+    {
+        int t[n+1][w+1];
+        
+        //Initialize matrix
+        for(int i=0; i<w+1; i++) t[0][i] = 0;
+        for(int j=0; j<n+1; j++) t[j][0] = 0;
+        
+        //Choice Diagram to code
+        for(int i = 1; i<n+1; i++){
+            for(int j=1; j<w+1; j++){
+                //1. In case of choice
+                if(wt[i-1] <= j){
+                    t[i][j] = max(val[i-1] + t[i-1][j-wt[i-1]], 0+t[i-1][j]);
+                }
+                //2. No choice
+                else if(wt[i-1] > j){
+                    t[i][j] = 0+t[i-1][j];
+                }
+            }
+        }
+        return t[n][w];
+    }
