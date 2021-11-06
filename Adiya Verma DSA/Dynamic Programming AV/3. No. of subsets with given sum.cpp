d9e08ca2be5949a60a3aaf377c7e5a44d724e13d
Mohit Jain <70198503+codedog001@users.noreply.github.com>
2021-11-06 15:49:28 +0530

    GFG Memoization

--- Adiya Verma DSA/Dynamic Programming AV/3. No. of subsets with given sum.cpp
+++ Adiya Verma DSA/Dynamic Programming AV/3. No. of subsets with given sum.cpp
@@ -1,5 +1,5 @@
 GFG	
-    
+    //1. Top Up (Memoization)
     int perfectSum(int arr[], int n, int sum){
 	    vector<vector<int>> t(n+1, vector<int>(sum+1, -1));
 	    return pum(arr, n, sum, t);
@@ -20,4 +20,27 @@ GFG
             return t[n][sum] = pum(arr, n-1, sum,t);
         }
         
+	}
+
+    //2. Bottom Down (Dynamic Programming)
+    	int perfectSum(int arr[], int n, int sum){
+	    //Bottom Up approach
+	    
+	    int t[n+1][sum+1];
+	    
+	    //Initialization
+	    for(int i=0; i<sum+1; i++) t[0][i] = 0;
+	    for(int i=0; i<n+1; i++) t[i][0] = 1;
+	    
+	    //Knapsack code
+	    for(int i=1; i<n+1; i++){
+	        for(int j=1; j<sum+1; j++){
+	            if(arr[i-1] <= j){
+	                t[i][j] = (t[i-1][j-arr[i-1]] + t[i-1][j]) % 1000000007;
+	            }else{
+	                t[i][j] = t[i-1][j] % 1000000007;
+	            }
+	        }
+	    }
+	    return t[n][sum] % 1000000007;
 	}
