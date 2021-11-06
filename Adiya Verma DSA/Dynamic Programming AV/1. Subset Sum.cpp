68eccc41044cfaf6719d908e1730935f36c0c369
Mohit Jain <70198503+codedog001@users.noreply.github.com>
2021-11-06 11:38:30 +0530

    Subset Sum Memoization

--- Adiya Verma DSA/Dynamic Programming AV/1. Subset Sum.cpp
+++ Adiya Verma DSA/Dynamic Programming AV/1. Subset Sum.cpp
@@ -1,4 +1,4 @@
-//1. Bottom Up approach
+//1. Bottom Up approach (Dynamic Programming)
 bool isSubsetSum(int n, int arr[], int sum){
         bool t[n+1][sum+1];
         
@@ -17,4 +17,27 @@ bool isSubsetSum(int n, int arr[], int sum){
             }
         }
         return t[n][sum];
+    }
+
+//2. Top Down Approach (Memoization)
+    bool isSubsetSum(int n, int arr[], int sum){
+       vector<vector<int>> t(n+1, vector<int>(sum+1, -1));
+       return helper(n,arr,sum,t);
+    }
+    
+    bool helper(int n, int arr[], int sum, vector<vector<int>>& t){
+         //Base case
+        if(sum==0) return 1;
+        if(n<=0) return 0;
+        
+        if(t[n][sum] != -1) return t[n][sum];
+        //Choice diagram
+        if(arr[n-1] <= sum){
+            t[n][sum] = helper(n-1, arr, sum-arr[n-1],t) || helper(n-1, arr, sum, t);
+            return t[n][sum];
+        }
+        else {
+            t[n][sum] = helper(n-1, arr, sum,t);
+            return t[n][sum];
+        }
     }
