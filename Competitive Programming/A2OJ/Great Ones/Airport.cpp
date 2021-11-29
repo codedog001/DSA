e5d7de1ba81a57307c627872ecce4ce60a1f50e1
Mohit Jain <70198503+codedog001@users.noreply.github.com>
2021-11-29 16:12:44 +0530

    CP

---
+++ Competitive Programming/A2OJ/Great Ones/Airport.cpp
@@ -0,0 +1,209 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vb = vector<bool>;
+using pii = pair<int, int>;
+using pll = pair<long, long>;
+using umi = unordered_map<int, int>;
+using umc = unordered_map<char, int>;
+using uml = unordered_map<long long, long long>;
+using usi = unordered_set<int>;
+using usl = unordered_set<long long>;
+using usc = unordered_set<char>;
+using uss = unordered_set<string>;
+
+// Vectors
+#define pb push_back
+#define all(a) a.begin(), a.end()
+
+// Pairs
+#define mp make_pair
+#define ff first
+#define ss second
+
+#define endl "\n"
+
+// Debugging header file
+#ifndef ONLINE_JUDGE
+#include <debug.hpp>
+#else
+#define dbg (...)
+#endif
+
+// Constants
+const int MOD = 1e9 + 7;                                // 998244353 (something FFT, idk yet)
+const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1}; // grids
+const double PI = acos((double)-1);
+const int MAX_N = 1e5 + 1;
+
+// Utility functions
+
+static inline void ltrim(std::string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }));
+}
+
+// trim from end (in place)
+static inline void rtrim(std::string &s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }).base(), s.end());
+}
+
+ll firstDigit(ll n){
+    string res = to_string(n);
+    return res[0]-48;
+}
+
+//Custom comparator for priority queue.
+class Custom{
+    public:
+    bool operator () (pii &a, pii &b){
+        
+        // cout << "Pair a: <" << a.first << "," << a.second << "> b: <" << b.first << "," << b.second << "> ";
+        //Compare frequency 
+        if(a.second > b.second) {
+            // cout << "true" << endl;
+            return true;
+        }
+        
+        return false;
+    }
+};
+
+bool doubleIsInt(double trouble) {
+   double absolute = abs( trouble );
+   return absolute == floor(absolute);
+}
+
+bool isPrime(ll n)
+{
+    // Corner case
+    if (n <= 1)
+        return false;
+  
+    // Check from 2 to n-1
+    for (ll i = 2; i < n; i++)
+        if (n % i == 0)
+            return false;
+  
+    return true;
+}
+
+// Binary Exponentiation
+long long binpow(long long a, long long b, long long m = MOD)
+{
+    a %= m;
+    long long res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
+
+// Fast Modulo - https://codeforces.com/blog/entry/72722?#comment-570232
+inline void addmod(int &a, int val, int p = MOD)
+{
+    if ((a = (a + val)) >= p)
+        a -= p;
+}
+inline void submod(int &a, int val, int p = MOD)
+{
+    if ((a = (a - val)) < 0)
+        a += p;
+}
+
+// // Graphs
+// vector<int> g[MAX_N];
+// vector<bool> vis(MAX_N);
+
+ll getMaxCost(ll n, vector<ll> a){
+    ll res = 0;
+    while(n!=0){
+        vector<ll>::iterator it;
+        it = max_element(a.begin(), a.end());
+        res += *it;
+        a[it-a.begin()] -= 1;
+        n-=1;
+    }
+    return res;
+}
+
+ll getMinCost(ll n, vector<ll> a){
+    ll res = 0;
+    while(n!=0){
+        for(int i=a.size()-1; i>=0; i--) {
+            res += a[i];
+            a[i] -= 1;
+            n-=1;
+            if(a[i]!=0) i++;
+            if(n<=0) break;
+        }
+    }
+    return res;
+}
+
+
+void solve() {
+
+    // Effort might not always be fruitful, but it's never wasted.
+    ll n, m, sum = 0;
+    cin >>n >> m;
+    vector<ll> a(m,0);
+    
+    for(int i=0; i<m; i++){
+        ll x; 
+        cin >> x;
+        a[i] = x;
+        sum += x;
+    }
+    
+    if(sum <= n){
+        ll res =0;
+        for(int i=0; i<a.size(); i++){
+            res += (a[i]*(a[i]+1))/2;
+        }
+        cout << res << " " << res <<  endl;
+    }else{
+        sort(a.begin(), a.end(), greater<int>());
+        
+        ll maxi = getMaxCost(n, a);
+        ll mini = getMinCost(n, a);
+        
+        cout << maxi << " " << mini << endl;
+    }
+    
+    
+    
+    
+    return;
+}
+
+
+/*  Few general reminders:
+    * Analyse constraints, look for bruteforce oppurtunities
+    * Check every assumption (trivial or non-trivial)
+    * int overflow, array bounds
+    * binpow uses 1e9 + 7 as MOD
+*/
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout << fixed << setprecision(20);
+
+    int t=-1;
+    // cin >> t;
+    // while (t--)
+        solve();
+}
