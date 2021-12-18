8502a01bec2b631eaf7c9b2af16c9ddec655e5c5
Mohit Jain <70198503+codedog001@users.noreply.github.com>
2021-12-18 15:39:11 +0530

    Add comments

--- Revise/Triplet Sum.cpp
+++ Revise/Triplet Sum.cpp
@@ -5,6 +5,10 @@ int fun(int* a, int k, int n, int low){
         if(a[low] + a[high] < k) low++;
         else if(a[low] + a[high] > k) high--;
         else{
+            //When we reach a[low] == a[high], we are sure, that we aren't missing anything 
+            //in the middle that could form a pair, because a[low] is equal to a[high] and hence we can increase
+            //result and return the answer.
+
             if(a[low] == a[high]){
                 int count = high-low+1;
                 result += (count *(count-1))/2;
@@ -20,7 +24,10 @@ int fun(int* a, int k, int n, int low){
                 highCount++;
             } 
             
-            result += lowCount * highCount;
+            result += lowCount * highCount; // NOt returning result immedeately when a[low] != a[high], 
+            //consider case: 033344445555 target: 8, a[i] = 0, k=8-0 = 8
+            //when we count 0,3 and 5 pair, if we return result here only, we will never be able count 0 and 4,4 pair 
+            //Basically, pair count might increase when a[low] != a[high], so until we reach low==high, we won't return result.
             
             low++;
             high--;
