d6d19b0e9df0f0315ed95bf4c504bd7fb2114c08
Mohit Jain <70198503+codedog001@users.noreply.github.com>
2021-12-28 18:54:35 +0530

    DP

--- Coding Ninjas/Dynamic Programming CN/Coin change Problem.cpp
+++ Coding Ninjas/Dynamic Programming CN/Coin change Problem.cpp
@@ -1,3 +1,7 @@
+// You are given an infinite supply of coins of each of denominations D = {D0, D1, D2, D3, ...... Dn-1}. You need to figure out the total number of ways W, in which you can make a change for Value V using coins of denominations D.
+// Note : Return 0, if change isn't possible.
+// W can be pretty large so output the answer % mod(10^9 + 7)
+
 ll findWays(ll v, vector<ll>& a, ll index, vector<vector<ll>>& dp){
     if(v<=0) return 1;
     if(index<=0) return 0;
