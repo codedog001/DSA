e4ab6d184ef330d63bd2b84a3acdffb14d985ce0
Mohit Jain <70198503+codedog001@users.noreply.github.com>
2022-01-04 12:24:30 +0530

    DP

--- Coding Ninjas/Dynamic Programming CN/Exceptional Problems/Non recursive DP thinking.cpp/(A++) Minimum Count.cpp
+++ Coding Ninjas/Dynamic Programming CN/Exceptional Problems/Non recursive DP thinking.cpp/(A++) Minimum Count.cpp
@@ -19,12 +19,13 @@ int main(){
         for(int i=5; i<=n; i++){
             int sqr = sqrt(i);
             int number = sqr * sqr;
-            if(i%number == 0) t[i] = 1;
+            if(i%number == 0) t[i] = (i/number); //t[i] != 1, try for test case: 8
             else{
                 int res = INT_MAX;
                 for(int j=sqr; j>=1; j--){
                     int newNumber = j * j;
                     res = min(i/newNumber + t[i%newNumber], res);
+                    if(res==2) break; //Can't be lesser than that.
                 }
                 t[i] = res;
             }
