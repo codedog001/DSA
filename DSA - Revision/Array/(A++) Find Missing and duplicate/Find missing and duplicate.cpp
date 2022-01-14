5b3e575b8c6c8b10d0afa15a7d191e25ef5607d6
Mohit Jain <70198503+codedog001@users.noreply.github.com>
2022-01-14 10:59:11 +0530

    Find missing and repeating

---
+++ DSA - Revision/Array/(A++) Find Missing and duplicate/Find missing and duplicate.cpp
@@ -0,0 +1,27 @@
+vector<int> Solution::repeatedNumber(const vector<int> &A) {
+        long long int len = A.size();
+
+    long long int S = (len * (len+1) ) /2;
+    long long int P = (len * (len +1) *(2*len +1) )/6;
+    long long int missingNumber=0, repeating=0;
+     
+     //This is the part where logic is applied, the vector contains integer, so at the time
+     //of multiplying (taking squares), typecast it to long long.
+
+    for(int i=0;i<A.size(); i++){
+       S -= (long long int)A[i];
+       P -= (long long int)A[i]*(long long int)A[i]; //Here.
+    }
+     
+    missingNumber = (S + P/S)/2;
+
+    repeating = missingNumber - S;
+
+    vector <int> ans;
+
+    ans.push_back(repeating);
+    ans.push_back(missingNumber);
+
+    return ans;
+
+}
