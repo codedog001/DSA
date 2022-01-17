5def1700450cb34a85d974238d92700e296ababd
Mohit Jain <70198503+codedog001@users.noreply.github.com>
2022-01-17 09:56:50 +0530

    Make code more readable

--- DSA - Revision/Binary Search/(A++++) Batteries Needed.cpp
+++ DSA - Revision/Binary Search/(A++++) Batteries Needed.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
     bool isValid(int n, long long mid, vector<int>& batteries){
-        long long totalBatteryNeeded = n * mid;
+        long long totalBatteryNeededForAllCompToRunAtMid = n * mid;
         long long sum = 0;
         for(long long num:batteries){
             sum += min(num, mid);
-            if(sum >= totalBatteryNeeded) return true;
+            if(sum >= totalBatteryNeededForAllCompToRunAtMid) return true;
         }
         return false;
     }
     
     long long maxRunTime(int n, vector<int>& batteries) {
         if(batteries.size() < n) return 0;
-        
-        long long no = batteries.size(), low = 0, high = 0, sum = 0, result = 0;
+        long long no = batteries.size(), minBatteryACompCanGet = 0, maxBatteryACompCanGet = 0, sum = 0, result = 0;
         sort(batteries.begin(), batteries.end());
-        
         for(auto& num:batteries) sum += num;
         
         //high = sum is also correct, but more precisely a single computer maximum can get
         //sum/n
-        high = sum/n, low = 0;
-        
-        while(low <= high){
-            long long mid = low + (high-low)/2;
+        maxBatteryACompCanGet = sum/n, minBatteryACompCanGet  = 0;
+        while(minBatteryACompCanGet <= maxBatteryACompCanGet){
+            long long mid = minBatteryACompCanGet + (maxBatteryACompCanGet - minBatteryACompCanGet)/2;
             if(isValid(n, mid, batteries)){
                 result = mid;
-                low = mid + 1;
-            }else high = mid - 1;
+                minBatteryACompCanGet = mid + 1;
+            }else maxBatteryACompCanGet = mid - 1;
         }
-        
         return result;
     }
 };
+
+
+
+
