f1ff3d705f3a7df2e8b6e78f9908ac48286db1d7
Mohit Jain <70198503+codedog001@users.noreply.github.com>
2022-01-22 15:07:12 +0530

    Course Schedule add BFS method

--- DSA - Revision/Graph/Course Schedule.cpp
+++ DSA - Revision/Graph/Course Schedule.cpp
@@ -1,5 +1,7 @@
 // Logic: If course 0 depends on 1, 1 depends on 2, and 2 depends on 0, there exists a cycle, and required number of courses can't be completed.
 
+1. Cycle Detection using DFS:
+
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
@@ -34,6 +36,56 @@ public:
             else if(curCallVisited[num] == 1) return true;
         }
         curCallVisited[node] = 0;
+        return false;
+    }
+};
+2. Cycle Detection using BFS: Kahn's Algorithm
+
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int n = numCourses;
+        vector<int> dirAdj[n];
+        //Create adj. matrix
+        for(auto& pairCourse: prerequisites){
+            int u = pairCourse[0], v = pairCourse[1];
+            dirAdj[u].push_back(v);
+        }
+        
+        //Check whether graph contains cycle using Kahn's algo
+        if(kahnsAlgoToFindCycle(dirAdj, n)) return false;
+        return true;
+    }
+    
+    bool kahnsAlgoToFindCycle(vector<int> dirAdj[], int n){
+        vector<int> inDegree(n,0), result;
+        //Store indegree of all nodes
+        for(int i=0; i<n; i++){
+            vector<int> t = dirAdj[i];
+            for(auto& num:t){
+                inDegree[num]++;
+            }
+        }
+        queue<int> q;
+        for(int i=0; i<n; i++){
+            if(inDegree[i] == 0) q.push(i);
+        }
+        
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            vector<int> neighbors = dirAdj[node];
+            result.push_back(node);
+            
+            for(auto& num: neighbors){
+                inDegree[num]--;
+                if(inDegree[num] == 0) q.push(num);
+            }
+        }
+        
+        //If graph contains cycle result will have < n elements
+        int resultSize = result.size();
+        if(resultSize < n) return true;
         return false;
     }
 };
