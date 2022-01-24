53e10b3d1822df427484731b0453aeefcedc273d
Mohit Jain <70198503+codedog001@users.noreply.github.com>
2022-01-24 11:22:39 +0530

    Redundant Connections

--- DSA - Revision/DFS & Backtracking/Max good people based on statements.cpp
+++ DSA - Revision/DFS & Backtracking/Max good people based on statements.cpp
@@ -1,7 +1,14 @@
-Logic: I have described the isValid() function with comments, and that is the main part for understanding this problem.
+Note: I have described the isValid() function with comments, and that is the main part for understanding this problem.
 Basically we are trying to create a configuration in cur of good or bad people by trying out all ways, then we are checking what we made is in accordance with what these people said (what good people said) is true or not. This part is described further in isValid().
 
 Why is it backtracking - Backtracking means to try out all possible ways, hence I think backtracking is correct word to describe what we are doing.
+
+Time and space complexity
+T.C: O(2^n * n^2) -
+-> 2^n - Create all possible combinations of size n.
+-> n^2 - Check validity of 2^n combinations each time by going through statements of all n persons in worst case leading to n^2.
+
+S.C: O(n) - Storing cur of size n.
 
 class Solution {
 public:
@@ -25,7 +32,7 @@ public:
         cur.push_back('1');
         dfs(statements, cur, index+1, goodPersonCount+1);
         
-        //Backtrack: Macke current person a bad person and call DFS
+        //Backtrack: Make current person a bad person and call DFS
         cur.back() = '0';
         dfs(statements, cur, index+1, goodPersonCount);
         
--- DSA - Revision/Graph/Redundant Connection.cpp
+++ DSA - Revision/Graph/Redundant Connection.cpp
@@ -1,8 +1,64 @@
+//1. Union by rank and Find with path compression:
+//T.C: O(n*α(n)) ≈ O(n)
+//Refer this for indepth explanation: https://leetcode.com/problems/redundant-connection/discuss/1295887/Easy-Solution-w-Explanation-or-All-Possible-Approaches-with-comments
+
+class DSU {
+    vector<int> parent, rank;
+    public:
+    DSU(int n){
+        rank.resize(n,0);
+        parent.resize(n,0);
+        iota(parent.begin(), parent.end(), 0);
+    }
+    
+    int findParent(int u){
+        if(u == parent[u]) return u;
+        return parent[u] = findParent(parent[u]); //Path compression
+    }
+    
+    void unionByRank(int u, int v){
+        int parU = findParent(u);
+        int parV = findParent(v);
+        
+        if(rank[parU] < rank[parV]){
+            parent[parU] = parV;
+        }
+        else if(rank[parV] < rank[parU]){
+            parent[parV] = parU;
+        }
+        else if(rank[parU] == rank[parV]){
+            parent[parU] = parV;
+            rank[parV] += 1;
+        }
+    }
+};
+
 class Solution {
 public:
-    vector<vector<int>> result;
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        n = edges.size();
+        //Since nodes are starting from 1, vectors will be of size n+1.
+        int size = n+1;
+        DSU ds(n+1);
+        for(auto& edge: edges){
+            int u = edge[0], v = edge[1];
+            int parU = ds.findParent(u);
+            int parV = ds.findParent(v);
+            
+            if(parU != parV) ds.unionByRank(u, v);
+            else return {u, v};
+        }
+        return {-1,-1};
+    }
+};
+
+// 2. DFS: Store all edges that cause cycle into a set, and go through the given edges from the end, return the one that matches.
+
+class Solution {
+public:
     vector<int> visited;
     int n = 0;
+    
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         n = edges.size();
         vector<int> undirAdjList[n+1];
@@ -16,32 +72,37 @@ public:
         
         int parent  = -1, source = 1;
         bool isCycle = false;
-        detectCycleDFS(source, parent, undirAdjList, isCycle);
-        cout << "size: " <<  result.size() << endl;
-        for(auto& t:result){
-            cout << t[0] << " - " << t[1] << endl;
+        detectCycleDFS(source, parent, undirAdjList);
+        
+        for(int i=n-1; i>=0; i--){
+            vector<int> v = edges[i];
+            if(st.find(v[0]) != st.end() && st.find(v[1]) != st.end()) return v;
         }
-        return {1,3};
+        return {-1,-1};
     }
     
     private:
-    void detectCycleDFS(int node, int parent, vector<int> undirAdjList[], bool& isCycle){
+    int cycleStartNode = -1;
+    bool isCycle = false;
+    unordered_set<int> st;
+    
+    void detectCycleDFS(int node, int parent, vector<int> undirAdjList[]){
+        if(visited[node] == 1){
+            isCycle = true;
+            cycleStartNode = node;
+            st.insert(node);
+            return;
+        }
         visited[node] = 1;
-        
         vector<int> neighbors = undirAdjList[node];
-        for(auto & neighbor: neighbors){
-            if(visited[neighbor] == 0) {
-                detectCycleDFS(neighbor, node, undirAdjList, isCycle);
-                if(isCycle && parent != -1){
-                    vector<int> temp = {min(node, neighbor), max(node, neighbor)};
-                    result.push_back(temp);
-                }
-            }
-            else if(visited[neighbor] == 1 && parent != neighbor){
-                vector<int> temp = {min(node, neighbor), max(node, neighbor)};
-                result.push_back(temp);
-                isCycle = true;
-            }
+        
+        for(auto& neighbor: neighbors){
+            if(neighbor == parent) continue;
+            
+            //DFS will be performed until cycle is not detected, once set starts getting filled no further nodes will be visited
+            if(st.empty()) detectCycleDFS(neighbor, node, undirAdjList);
+            if(isCycle) st.insert(node);
+            if(cycleStartNode == node) isCycle = false;
         }
     }
 };
